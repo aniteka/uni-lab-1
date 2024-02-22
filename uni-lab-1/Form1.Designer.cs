e201bd3c73b8249f7fc1506e071367c7bdb0d9c6
Nazariy <266444833552m@gmail.com>
2024-02-23 00:30:50 +0300

    final version

--- uni-lab-1/Form1.Designer.cs
+++ uni-lab-1/Form1.Designer.cs
@@ -29,29 +29,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.studentsGrid = new System.Windows.Forms.DataGridView();
+            this.studentDataGrid_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentDataGrid_markA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentDataGrid_markB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentDataGrid_markC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentDataGrid_markD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentDataGrid_markF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentsDataSave = new System.Windows.Forms.Button();
+            this.studentsDataLoad = new System.Windows.Forms.Button();
+            this.studentsGenerateLessMarkMenu = new System.Windows.Forms.Button();
+            this.lessMarkMenu = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsGrid)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // studentsGrid
             // 
-            this.label1.Location = new System.Drawing.Point(45, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 40);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Hello World";
+            this.studentsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentsGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.studentDataGrid_Name, this.studentDataGrid_markA, this.studentDataGrid_markB, this.studentDataGrid_markC, this.studentDataGrid_markD, this.studentDataGrid_markF });
+            this.studentsGrid.Location = new System.Drawing.Point(12, 12);
+            this.studentsGrid.Name = "studentsGrid";
+            this.studentsGrid.Size = new System.Drawing.Size(295, 426);
+            this.studentsGrid.TabIndex = 2;
+            // 
+            // studentDataGrid_Name
+            // 
+            this.studentDataGrid_Name.HeaderText = "Name";
+            this.studentDataGrid_Name.Name = "studentDataGrid_Name";
+            // 
+            // studentDataGrid_markA
+            // 
+            this.studentDataGrid_markA.HeaderText = "A";
+            this.studentDataGrid_markA.Name = "studentDataGrid_markA";
+            this.studentDataGrid_markA.Width = 30;
+            // 
+            // studentDataGrid_markB
+            // 
+            this.studentDataGrid_markB.HeaderText = "B";
+            this.studentDataGrid_markB.Name = "studentDataGrid_markB";
+            this.studentDataGrid_markB.Width = 30;
+            // 
+            // studentDataGrid_markC
+            // 
+            this.studentDataGrid_markC.HeaderText = "C";
+            this.studentDataGrid_markC.Name = "studentDataGrid_markC";
+            this.studentDataGrid_markC.Width = 30;
+            // 
+            // studentDataGrid_markD
+            // 
+            this.studentDataGrid_markD.HeaderText = "D";
+            this.studentDataGrid_markD.Name = "studentDataGrid_markD";
+            this.studentDataGrid_markD.Width = 30;
+            // 
+            // studentDataGrid_markF
+            // 
+            this.studentDataGrid_markF.HeaderText = "F";
+            this.studentDataGrid_markF.Name = "studentDataGrid_markF";
+            this.studentDataGrid_markF.Width = 30;
+            // 
+            // studentsDataSave
+            // 
+            this.studentsDataSave.Location = new System.Drawing.Point(313, 12);
+            this.studentsDataSave.Name = "studentsDataSave";
+            this.studentsDataSave.Size = new System.Drawing.Size(75, 23);
+            this.studentsDataSave.TabIndex = 3;
+            this.studentsDataSave.Text = "SAVE";
+            this.studentsDataSave.UseVisualStyleBackColor = true;
+            this.studentsDataSave.Click += new System.EventHandler(this.studentsDataSave_Click);
+            // 
+            // studentsDataLoad
+            // 
+            this.studentsDataLoad.Location = new System.Drawing.Point(313, 41);
+            this.studentsDataLoad.Name = "studentsDataLoad";
+            this.studentsDataLoad.Size = new System.Drawing.Size(75, 23);
+            this.studentsDataLoad.TabIndex = 4;
+            this.studentsDataLoad.Text = "LOAD";
+            this.studentsDataLoad.UseVisualStyleBackColor = true;
+            this.studentsDataLoad.Click += new System.EventHandler(this.studentsDataLoad_Click);
+            // 
+            // studentsGenerateLessMarkMenu
+            // 
+            this.studentsGenerateLessMarkMenu.Location = new System.Drawing.Point(313, 70);
+            this.studentsGenerateLessMarkMenu.Name = "studentsGenerateLessMarkMenu";
+            this.studentsGenerateLessMarkMenu.Size = new System.Drawing.Size(75, 23);
+            this.studentsGenerateLessMarkMenu.TabIndex = 5;
+            this.studentsGenerateLessMarkMenu.Text = "GENERATE";
+            this.studentsGenerateLessMarkMenu.UseVisualStyleBackColor = true;
+            this.studentsGenerateLessMarkMenu.Click += new System.EventHandler(this.studentsGenerateLessMarkMenu_Click);
+            // 
+            // lessMarkMenu
+            // 
+            this.lessMarkMenu.Location = new System.Drawing.Point(394, 12);
+            this.lessMarkMenu.Name = "lessMarkMenu";
+            this.lessMarkMenu.Size = new System.Drawing.Size(222, 426);
+            this.lessMarkMenu.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(628, 450);
+            this.Controls.Add(this.lessMarkMenu);
+            this.Controls.Add(this.studentsGenerateLessMarkMenu);
+            this.Controls.Add(this.studentsDataLoad);
+            this.Controls.Add(this.studentsDataSave);
+            this.Controls.Add(this.studentsGrid);
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
-            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.studentsGrid)).EndInit();
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lessMarkMenu;
+
+        private System.Windows.Forms.Button studentsGenerateLessMarkMenu;
+
+        private System.Windows.Forms.Button studentsDataSave;
+        private System.Windows.Forms.Button studentsDataLoad;
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentDataGrid_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentDataGrid_markA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentDataGrid_markB;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentDataGrid_markC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentDataGrid_markD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentDataGrid_markF;
+
+        private System.Windows.Forms.DataGridView studentsGrid;
 
         #endregion
     }
--- uni-lab-1/Form1.cs
+++ uni-lab-1/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,101 @@ namespace uni_lab_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void studentsDataSave_Click(object sender, EventArgs e)
+        {
+            using (var fin = File.CreateText("FILE.txt"))
+            {
+                foreach (DataGridViewRow row in studentsGrid.Rows)
+                {
+                    foreach (DataGridViewCell rowCell in row.Cells)
+                    {
+                        if (rowCell.Value == null && rowCell.OwningColumn == studentDataGrid_Name)
+                            break;
+
+                        if (rowCell.Value != null)
+                        {
+                            fin.WriteLine(rowCell.Value.ToString());
+                        }
+                        else
+                        {
+                            fin.WriteLine("0\n");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void studentsDataLoad_Click(object sender, EventArgs e)
+        {
+            studentsGrid.Rows.Clear();
+            using (var fout = File.OpenText("FILE.txt"))
+            {
+                while (!fout.EndOfStream)
+                {
+                    var name = fout.ReadLine();
+                    var markA = int.Parse(fout.ReadLine());
+                    var markB = int.Parse(fout.ReadLine());
+                    var markC = int.Parse(fout.ReadLine());
+                    var markD = int.Parse(fout.ReadLine());
+                    var markF = int.Parse(fout.ReadLine());
+                    studentsGrid.Rows.Add(name, markA, markB, markC, markD, markF);
+                }
+            }
+        }
+
+        private void studentsGenerateLessMarkMenu_Click(object sender, EventArgs e)
+        {
+            var lessMark = new (string name, int mark)[5];
+            for (var i = 0; i < lessMark.Length; i++)
+            {
+                lessMark[i].mark = int.MaxValue;
+            }
+            
+            foreach (DataGridViewRow row in studentsGrid.Rows)
+            {
+                string name = "None";
+                int i = 0;
+                foreach (DataGridViewCell rowCell in row.Cells)
+                {
+                    if (rowCell.Value == null && rowCell.OwningColumn == studentDataGrid_Name)
+                        break;
+                    
+                    if (rowCell.OwningColumn == studentDataGrid_Name)
+                    {
+                        name = rowCell.Value.ToString();
+                        continue;
+                    }
+
+                    int mark;
+                    if (rowCell.Value == null)
+                    {
+                        mark = 0;
+                    }
+                    else
+                    {
+                        if (!int.TryParse(rowCell.Value.ToString(), out mark))
+                            mark = 0;
+                    }
+
+                    if (lessMark[i].mark > mark)
+                    {
+                        lessMark[i].name = name;
+                        lessMark[i].mark = mark;
+                    }
+
+                    ++i;
+                }
+            }
+
+            var resultStringBuilder = new StringBuilder();
+            resultStringBuilder.AppendLine($"A: {lessMark[0].name}");
+            resultStringBuilder.AppendLine($"B: {lessMark[1].name}");
+            resultStringBuilder.AppendLine($"C: {lessMark[2].name}");
+            resultStringBuilder.AppendLine($"D: {lessMark[3].name}");
+            resultStringBuilder.AppendLine($"F: {lessMark[4].name}");
+            lessMarkMenu.Text = resultStringBuilder.ToString();
         }
     }
 }
